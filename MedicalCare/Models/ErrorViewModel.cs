a542094dcf1724d52f12224f15fe8e50846936a3
Manelisi <127588093+Manelisy@users.noreply.github.com>
2026-02-12 20:00:35 +0200

    Initial commit

---
+++ MedicalCare/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MedicalCare.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MedicalCare/README.md
@@ -0,0 +1,2 @@
+# MedClinic-Group-Project-2024
+Hospital Ward Management System
